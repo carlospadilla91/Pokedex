fd3895ded25c167b1ac4151df593b3c2e0b7ea17
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-09-13 15:17:19 -0500

    feat: pokemon html display complete

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 "use strict"
 
+const pokedex = document.getElementById("pokedex");
+
 // iterating 1-150 of the pokemon and pushing each one to empty array named promises
 const getPokemon = () => {
     const promises = [];
     for (let i = 1; i <= 150; i++) {
-        const apiUrl = `https://pokeapi.co/api/v2/${i}`;
+        const apiUrl = `https://pokeapi.co/api/v2/pokemon/${i}`;
         promises.push(fetch(apiUrl).then(res => res.json()));
     }
     Promise.all(promises).then((results) => {
@@ -19,8 +21,20 @@ const getPokemon = () => {
 }
 
 const displayPokemon = (pokemon) => {
+    const html = pokemon
+        .map((pokeman) => `
+        <li class="card">
+            <img class="card-image" src="${pokeman.image}"/>
+            <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+            <p class="card-subtitle">Type: ${pokeman.type}</p>
+        </li>
+    `
+        )
+        .join("");
+    pokedex.innerHTML = html;
+};
 
-}
+getPokemon();
 
 
 
