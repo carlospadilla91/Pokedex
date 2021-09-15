00edc35810479cb91a15be6465669a1b997a095a
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-09-15 13:14:32 -0500

    feat: added some style to cards

--- js/main.js
+++ js/main.js
@@ -7,19 +7,22 @@ let pokemonCharacters = [];
 
 userSearch.keyup((e) => {
     const searchString = e.target.value;
+    console.log(searchString)
     const filteredPokemon = pokemonCharacters.filter((character) => {
-        return character.name.includes(searchString);
+        console.log(character);
+        return (character.name.includes(searchString) || character.type.includes(searchString));
     })
+    displayPokemon(filteredPokemon);
 })
 
 // iterating 1-150 of the pokemon and pushing each one to empty array named promises
 const getPokemon = () => {
-    const promises = [];
+    pokemonCharacters = [];
     for (let i = 1; i <= 150; i++) {
         const apiUrl = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        promises.push(fetch(apiUrl).then(res => res.json()));
+        pokemonCharacters.push(fetch(apiUrl).then(res => res.json()));
     }
-    Promise.all(promises).then((results) => {
+    Promise.all(pokemonCharacters).then((results) => {
         const pokemon = results.map((result) => ({
             name: result.name,
             image: result.sprites['front_default'],
@@ -30,6 +33,21 @@ const getPokemon = () => {
     });
 }
 
+// const fetchPokemon = async () => {
+//     const url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
+//     const res = await fetch(url);
+//     pokemonCharacters = await res.json();
+//     console.log(pokemonCharacters);
+//     const pokemon = pokemonCharacters.results.map((data, index) => ({
+//         name: data.name,
+//         id: index + 1,
+//         type: data.types.map((type) => type.type.name).join(", "),
+//         image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+//
+//     }))
+//     displayPokemon(pokemon);
+// }
+
 const displayPokemon = (pokemon) => {
     const html = pokemon
         .map((pokeman) => `
@@ -38,7 +56,7 @@ const displayPokemon = (pokemon) => {
         <div class="card-body">
             <h5 class="card-title">${pokeman.id}. ${pokeman.name}</h5>
             <p class="card-text">Type: ${pokeman.type}</p>
-            <a href="#" class="btn btn-primary" id="single-view">Go somewhere</a>
+<!--            <a href="#" class="btn btn-primary" id="single-view">Go somewhere</a>-->
         </div>
 </div>
     `
@@ -48,7 +66,8 @@ const displayPokemon = (pokemon) => {
 };
 
 getPokemon();
-
+// fetchPokemon();
+// console.log(pokemonCharacters)
 
 
 
